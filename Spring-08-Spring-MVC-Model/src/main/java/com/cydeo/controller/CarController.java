32468cf7b49654579128b53cc87cfc3ac3c96fc5
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-09 16:07:12 -0500

    using path parameters dynamic

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,5 +24,13 @@ public class CarController {
         model.addAttribute("make", make);
         //model.addAttribute("year",year);
         return "car-info";
+    }
+    //localhost:8080/car/info/Toyota
+    @RequestMapping("/info/{make}/{year}")  //using path parameter, since it's dynamic we put it in curly braces
+    public String getCarInfo(@PathVariable String make,@PathVariable Integer year, Model model){
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+        return "car-info";
+
     }
 }
