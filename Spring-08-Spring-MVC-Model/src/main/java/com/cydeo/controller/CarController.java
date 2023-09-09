06adc775fc4eaa19767ead88969a647ab99b82c8
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-09 16:00:04 -0500

    using query parameters

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -1,9 +1,27 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping
+@RequestMapping("/car")
 public class CarController {
+
+    @RequestMapping("/info") //localhost:8080/car/info?make=Honda&model=Civic
+    public String carInfo(@RequestParam String make,Integer year, Model model){
+        System.out.println(make);
+        model.addAttribute("make", make);
+        model.addAttribute("year",year);
+        return "car-info";
+    }
+
+    @RequestMapping("/info2") //localhost:8080/car/info2  --> can put query parameter or just leave, and default value will shown
+    public String carInfo2(@RequestParam(value = "make",required = false, defaultValue = "Tesla") String make,Model model){
+        System.out.println(make);
+        model.addAttribute("make", make);
+        //model.addAttribute("year",year);
+        return "car-info";
+    }
 }
