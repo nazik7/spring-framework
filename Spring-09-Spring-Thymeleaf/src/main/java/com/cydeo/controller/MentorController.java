623b5a380687359429b9bef5d7a8ad50a17cd760
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-22 04:36:30 -0500

    created mentor-register html file and controller

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,17 @@
+package com.cydeo.controller;
+
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+    @GetMapping("/register")
+    public String registerMentor(Model model){
+        model.addAttribute("mentor", new Mentor());
+        return "mentor/mentor-register";
+    }
+}
