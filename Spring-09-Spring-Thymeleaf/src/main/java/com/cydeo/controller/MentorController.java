6702a53d7184327b115af8b15dbb9a3e51057bb4
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-22 05:04:50 -0500

    used Model object to hold java objects and pass to view

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -6,12 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
     @GetMapping("/register")
     public String registerMentor(Model model){
         model.addAttribute("mentor", new Mentor());
+        List<String> batchList = Arrays.asList("JD1","JD2","EU1","EU2","B18","B19");
+        model.addAttribute("batchList",batchList);
         return "mentor/mentor-register";
     }
 }
