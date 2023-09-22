0c2f5062e6f556c2e25a5aed96ccb60514279197
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-22 12:39:36 -0500

    used @ModelAttribute to pass java object to view and used @PostMapping

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -18,5 +20,12 @@ public class MentorController {
         List<String> batchList = Arrays.asList("JD1","JD2","EU1","EU2","B18","B19");
         model.addAttribute("batchList",batchList);
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String registerMentor2(@ModelAttribute("mentor")Mentor mentor, Model model){
+        System.out.println(mentor.toString()); //getting mentor object from the view(html) that user input
+        //return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 }
