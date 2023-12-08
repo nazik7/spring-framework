ea061f06801f49fac7db56adb880d1e1966d63a9
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-12-08 10:14:13 -0600

    starting API with Spring

--- Spring-15-REST/src/main/java/repository/CourseRepository.java
+++ Spring-15-REST/src/main/java/repository/CourseRepository.java
@@ -3,5 +3,8 @@ package repository;
 import entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findAllByCategory(String category);
 }
