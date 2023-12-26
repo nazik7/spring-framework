af0a477b36048fe746a41c90dd993a391bb5e26f
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-12-26 06:15:44 -0600

    Consuming Api with FeignClient

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Concume_FeignClient.java
+++
@@ -1,9 +0,0 @@
-package com.cydeo.controller;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-@SpringBootApplication
-@EnableFeignClients
-public class Concume_FeignClient {
-
-}
