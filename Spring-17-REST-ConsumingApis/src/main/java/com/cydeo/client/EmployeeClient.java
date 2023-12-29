199ddf4ef050209fa2743123ffab79e926a36344
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-12-29 13:49:59 -0600

    Consuming Api with FeignClient, need to pay attention to Spring Boot version and Spring Cloud version for compatability

---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/client/EmployeeClient.java
@@ -0,0 +1,16 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
+
+@FeignClient(url = "https://dummyapi.io", name = "EMPLOYEE-CLIENT")
+public interface EmployeeClient {
+
+    @GetMapping("/data/v1/user?limit=10")
+    public Employee getEmployee(@RequestHeader("app-id")String id);
+
+}
